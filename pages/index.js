d70f8f2c5eebf61b9785e65208cf80de21013a4e
Linkai Wu <linkai861@gmail.com>
2021-04-18 02:18:07 +0000

    Remove extraneous text element in index.js

--- pages/index.js
+++ pages/index.js
@@ -27,32 +27,30 @@ export default function Home() {
         </Link> by linkai101
       </Heading>
 
-      <Text my={8} mt={12}>
-        <Heading as="h3" size="md" my={2}>
-          Things to note:
-        </Heading>
-        <UnorderedList px={16} align="left">
-          <ListItem>
-            The <Code>.gitkeep</Code> in <Code>/public</Code> is there to prevent GitHub from deleting the <Code>/public</Code> directory. Delete it when adding any files to it.
-          </ListItem>
-          <ListItem>
-            Add a favicon by inserting an image named 'favicon.ico' into <Code>/public</Code> 
-            (favicon import line is in <Code>/pages/_app.js</Code>)
-          </ListItem>
-          <ListItem>
-            Update color theme in <Code>/lib/theme.js</Code>.
-            Once finished, make sure to uncomment 'color' in
-            the theme import on line 35.
-          </ListItem>
-          <ListItem>
-            A ColorModeToggle component has been included
-            in <Code>/components/ColorModeToggle.js</Code>
-            for toggling color mode. <Link href="https://chakra-ui.com/docs/features/color-mode" isExternal>(see more details)</Link> Edit color mode options in <Code>/lib/theme.js</Code>
-          </ListItem>
-        </UnorderedList>
-      </Text>
+      <Heading as="h3" size="md" my={2} mt={8}>
+        Things to note:
+      </Heading>
+      <UnorderedList px={16} align="left">
+        <ListItem>
+          The <Code>.gitkeep</Code> in <Code>/public</Code> is there to prevent GitHub from deleting the <Code>/public</Code> directory. Delete it when adding any files to it.
+        </ListItem>
+        <ListItem>
+          Add a favicon by inserting an image named 'favicon.ico' into <Code>/public</Code> 
+          (favicon import line is in <Code>/pages/_app.js</Code>)
+        </ListItem>
+        <ListItem>
+          Update color theme in <Code>/lib/theme.js</Code>.
+          Once finished, make sure to uncomment 'color' in
+          the theme import on line 35.
+        </ListItem>
+        <ListItem>
+          A ColorModeToggle component has been included
+          in <Code>/components/ColorModeToggle.js</Code>
+          for toggling color mode. <Link href="https://chakra-ui.com/docs/features/color-mode" isExternal>(see more details)</Link> Edit color mode options in <Code>/lib/theme.js</Code>
+        </ListItem>
+      </UnorderedList>
 
-      <Heading as="h3" size="md">Happy hacking!</Heading>
+      <Heading as="h3" size="md" mt={6}>Happy hacking!</Heading>
     </Container>
   );
 }
